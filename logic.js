f3abc44b93df38c1336c518dc927a1ec4af7edd1
charlie-daniels <charlied030401@gmail.com>
2022-08-20 23:12:34 +0100

    add computer guess

---
+++ logic.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    choice =  Math.floor(Math.random() * 3);
+
+    switch(choice) {
+    case 0:
+        return 'Rock';
+    case 1:
+        return 'Paper';
+    case 2:
+        return 'Scissors';
+    default:
+        return '';
+    }
+}
+
+console.log(getComputerChoice());
