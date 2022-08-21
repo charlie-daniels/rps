5a76e2242fd55167f476e2e83c76a3be37a4926a
charlie-daniels <charlied030401@gmail.com>
2022-08-21 11:26:56 +0100

    add win cases

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    choice =  Math.floor(Math.random() * 3);
+    let choice = Math.floor(Math.random() * 3);
 
     switch(choice) {
     case 0:
@@ -13,4 +13,27 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    
+    if(playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+        return 'You lose! Paper beats rock!';
+    } else if(playerSelection === 'ROCK' && computerSelection === 'ROCK') {
+        return 'Draw!';
+    } else if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
+        return 'You win! Rock beats scissors!';
+    } else if(playerSelection === 'PAPER' && computerSelection === 'PAPER') {
+        return 'Draw!';
+    } else if(playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+        return 'You win! Paper beats rock!';
+    } else if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+        return 'You lose! Scissors beats paper!';
+    } else if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return 'You win! Scissors beats paper!';
+    } else if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+        return 'You lose! Rock beats scissors!';
+    } else if(playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS') {
+        return 'Draw!';
+    }
+}
+
