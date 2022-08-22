cdd9d243958b142acf92b3c701f1f4a5d8f8dccd
charlie-daniels <charlied030401@gmail.com>
2022-08-22 19:07:09 +0100

    add scores

--- logic.js
+++ logic.js
@@ -17,19 +17,19 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     
     if(playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return 'You lose! Paper beats rock!';
+        return -1 //'You lose! Paper beats rock!';
     } else if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
-        return 'You win! Rock beats scissors!';
+        return 1 //'You win! Rock beats scissors!';
     } else if(playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        return 'You win! Paper beats rock!';
+        return 1 //'You win! Paper beats rock!';
     } else if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return 'You lose! Scissors beats paper!';
+        return -1 //'You lose! Scissors beats paper!';
     } else if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return 'You win! Scissors beats paper!';
+        return 1 //'You win! Scissors beats paper!';
     } else if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return 'You lose! Rock beats scissors!';
+        return -1 //'You lose! Rock beats scissors!';
     } else {
-        return 'Draw!';
+        return 0 //'Draw!';
     }
 }
 
@@ -37,28 +37,46 @@ const divResults = document.querySelector('.results');
 const divScore = document.querySelector(".score");
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach(b => b.addEventListener('click', () => {
-    let player = b.textContent;
-    let computer = getComputerChoice();
-    let result = playRound(player, computer);
-
-    divResults.textContent = result;
-}));
-
 let scorePlayer = 0;
 let scoreComputer = 0;
 
-divScore.textContent = `${scorePlayer} - ${scoreComputer}`;
+function applyScore(score){
+    if(score > 0){
+        scorePlayer++;
+    } else if(score < 0){
+        scoreComputer++;
+    }
+    divScore.textContent = `${scorePlayer} - ${scoreComputer}`;
+}
+
+function translateResult(player, computer, score) {
+    if (score > 0) {
+        return `You win! ${player} beats ${computer}`;
+    } else if (score < 0) {
+        return `You lose! ${computer} beats ${player}`;
+    } else {
+        return 'Draw!';
+    }
+}
+
+buttons.forEach(b => b.addEventListener('click', () => {
+    let player = b.textContent;
+    let computer = getComputerChoice();
+    let score = playRound(player, computer);
+    applyScore(score);
+    result = translateResult(player, computer, score);
+    divResults.textContent = result;
+
+    if (scorePlayer === 5) {
+        divResults.textContent = "Player wins!";
+        resetScores();
+    } else if (scoreComputer === 5){
+        divResults.textContent = "Computer wins!";
+        resetScores();
+    }
+}));
 
 function resetScores() {
     scorePlayer = 0;
     scoreComputer = 0;
-}
-
-if (scorePlayer === 5) {
-    divResults.textContent = "Player wins!";
-    resetScores();
-} else if (scoreComputer === 5){
-    divResults.textContent = "Computer wins!";
-    resetScores();
 }
