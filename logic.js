924ff5d3c3294b824909fc81082ad874556f6c3a
charlie-daniels <charlied030401@gmail.com>
2022-08-22 19:11:23 +0100

    refactor

--- logic.js
+++ logic.js
@@ -17,19 +17,19 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     
     if(playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return -1 //'You lose! Paper beats rock!';
+        return -1;
     } else if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
-        return 1 //'You win! Rock beats scissors!';
+        return 1; 
     } else if(playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        return 1 //'You win! Paper beats rock!';
+        return 1;
     } else if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return -1 //'You lose! Scissors beats paper!';
+        return -1;
     } else if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return 1 //'You win! Scissors beats paper!';
+        return 1;
     } else if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return -1 //'You lose! Rock beats scissors!';
+        return -1;
     } else {
-        return 0 //'Draw!';
+        return 0;
     }
 }
 
@@ -59,8 +59,12 @@ function translateResult(player, computer, score) {
     }
 }
 
-buttons.forEach(b => b.addEventListener('click', () => {
-    let player = b.textContent;
+function resetScores() {
+    scorePlayer = 0;
+    scoreComputer = 0;
+}
+
+function playGame(player){
     let computer = getComputerChoice();
     let score = playRound(player, computer);
     applyScore(score);
@@ -74,9 +78,11 @@ buttons.forEach(b => b.addEventListener('click', () => {
         divResults.textContent = "Computer wins!";
         resetScores();
     }
+}
+
+buttons.forEach(b => b.addEventListener('click', () => {
+    let player = b.textContent;
+    playGame(player);
 }));
 
-function resetScores() {
-    scorePlayer = 0;
-    scoreComputer = 0;
-}
+
